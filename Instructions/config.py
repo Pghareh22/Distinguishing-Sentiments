9ba8ddfa8c3c9fe5da691942d5ab2f3d3164a22a
Pghareh22 <38015826+Pghareh22@users.noreply.github.com>
2018-09-25 00:11:11 -0700

    Update config.py

--- Instructions/config.py
+++ Instructions/config.py
@@ -1,5 +1,5 @@
 # Twitter API Keys
-consumer_key = "I9jjDK5SfS4cbXFXpA7bOgZSl"
-consumer_secret = "4lhh6OBAbSCxSAq7RixmNXoJdxkjlGeSg5CNr8dkvoMZaiKIcg"
-access_token = "4328366783-9sllBfWS93h5CaBvaMrKPlFyTCd7YO6qjqBtjKY"
-access_token_secret = "uLFOIEzP7qr5pIuTaFvJQ1ZQqgLl5kYbtcR6WFibSiWSI"
+consumer_key = 
+consumer_secret = 
+access_token = 
+access_token_secret = 
